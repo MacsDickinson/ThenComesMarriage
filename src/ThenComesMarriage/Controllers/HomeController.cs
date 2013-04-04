12ae13ca07b990bacee7f461c5761b1a1bc3659d
MacsDickinson <macsyd@hotmail.co.uk>
2013-04-04 23:33:07 +0100

    added a default view so we can run it

---
+++ src/ThenComesMarriage/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Raven.Client;
+
+namespace ThenComesMarriage.Controllers
+{
+    public class HomeController : Controller
+    {
+		private readonly IDocumentSession _documentSession;
+		public HomeController(IDocumentSession documentSession)
+		{
+			_documentSession = documentSession;
+		}
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
