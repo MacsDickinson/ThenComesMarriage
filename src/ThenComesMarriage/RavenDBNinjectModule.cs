169a55c8a1b6ba4fe2eab5da863d13fc784ccd7f
MacsDickinson <macsyd@hotmail.co.uk>
2013-04-04 23:11:10 +0100

    Added RavenDB and Ninject

---
+++ src/ThenComesMarriage/RavenDBNinjectModule.cs
@@ -0,0 +1,24 @@
+﻿using Ninject;
+using Ninject.Modules;
+using Ninject.Web.Common;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace ThenComesMarriage
+{
+	public class RavenDBNinjectModule : NinjectModule
+	{
+		public override void Load()
+		{
+			Bind<IDocumentStore>()
+				.ToMethod(context =>
+					          {
+						          var documentStore = new DocumentStore();
+						          return documentStore.Initialize();
+					          })
+				.InSingletonScope();
+
+			Bind<IDocumentSession>().ToMethod(context => context.Kernel.Get<IDocumentStore>().OpenSession()).InRequestScope();
+		}
+	}
+}
