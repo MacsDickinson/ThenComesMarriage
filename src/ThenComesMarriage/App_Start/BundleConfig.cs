97c0b59344d7f671be63f39938dfcaf1ab5036fd
MacsDickinson <macsyd@hotmail.co.uk>
2013-04-20 15:49:55 +0100

    Added script and style bundling

---
+++ src/ThenComesMarriage/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Optimization;
+
+namespace ThenComesMarriage.App_Start
+{
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+						"~/Scripts/jquery-{version}.js"));
+			
+			bundles.Add(new ScriptBundle("~/bundles/kube").Include(
+				 "~/Scripts/kube.buttons.js",
+				 "~/Scripts/kube.tabs.js")
+			);
+
+			bundles.Add(new StyleBundle("~/kube").Include("~/Style/kube.css"));
+			bundles.Add(new StyleBundle("~/css").Include("~/Style/master.css"));
+
+		}
+	}
+}
