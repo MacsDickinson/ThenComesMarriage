e79ebd805891f4d31843cb09bcb0e7783c69d35b
MacsDickinson <macsyd@hotmail.co.uk>
2013-04-20 16:02:45 +0100

    Hooked in bundles

--- src/ThenComesMarriage/Global.asax.cs
+++ src/ThenComesMarriage/Global.asax.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using ThenComesMarriage.App_Start;
 
 namespace ThenComesMarriage
 {
@@ -19,6 +17,7 @@ namespace ThenComesMarriage
 			WebApiConfig.Register(GlobalConfiguration.Configuration);
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
 		}
 	}
 }
