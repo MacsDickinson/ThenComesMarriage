9dcaa8e2afd57275136a74758bdfbcff54098251
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-02-20 12:03:42 +0000

    added blank site

---
+++ src/ThenComesMarriage/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ThenComesMarriage.Startup))]
+
+namespace ThenComesMarriage
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
